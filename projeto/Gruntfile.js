c1db9eefb829d4be163dc18fa98c5a260b14b06d
Estevão <estevaovasques@hotmail.com>
2016-12-26 12:50:12 -0200

    Primeiro commit

---
+++ projeto/Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function (grunt){
+
+	grunt.initConfig({
+
+		copy: {
+			public: {
+				expand: true,
+				cwd: 'public',
+				src: '**',
+				dest: 'dist'
+			}
+		},
+		clean:{
+			dist: {
+				src: 'dist'
+			}
+		},
+		useminPrepare: {
+			html: 'dist/**/*.html'
+		},
+		usemin: {
+			html: 'dist/**/*.html'
+		}
+
+	});
+
+	grunt.registerTask('dist', ['clean', 'copy']);
+	grunt.registerTask('minifica', ['useminPrepare', 
+		'concat', 'uglify', 'cssmin', 'usemin' ]);
+
+	grunt.registerTask('default', ['dist', 'minifica']);
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-usemin');
+};
